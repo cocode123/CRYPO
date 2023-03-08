9fb464e88be01df79502617b0889ffb5339ef217
cocode123 <927862925@qq.com>
2023-03-09 00:44:09 +0800

    v1.0.0--获取MD5修复了bug

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 char table[] = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' };
 
 int main() {
-    MD5 md5;
+    MD5 md5;//–ﬁ∏¥¡Àbug
     //–ﬁ∏¥¡Àbug
     std::cout<<md5.startMD5("hello world")<<std::flush;
     return 0;
