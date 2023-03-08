76ac219444024e0210497b2c88f53c2bd89f2e12
cocode123 <927862925@qq.com>
2023-03-09 01:12:06 +0800

    修复8个地方的bug

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
+//111
 #include "MD5.h"
+//222
 char table[] = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' };
-
+//333
 int main() {
+//444
+//555
     MD5 md5;
+//666
     //–ﬁ∏¥¡Àbug
+//777
     std::cout<<md5.startMD5("hello world")<<std::flush;
+//888
     return 0;
 
 }
